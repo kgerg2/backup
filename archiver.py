fc84a77510eebcf5b170630c9d06ad6e08c3fa6a
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2022-05-26 22:36:28 +0200

    Archiváláshoz összehasonlítás az rclone-nal

--- archiver.py
+++ archiver.py
@@ -1,8 +1,13 @@
 from datetime import datetime, timedelta
+import json
+import logging
 from pathlib import Path
+from shutil import copy2
+import subprocess
+from tempfile import TemporaryDirectory
 from zoneinfo import ZoneInfo
 
-from util import get_file_info, is_same_file
+from util import get_file_info, write_checkfile
 
 
 LOCAL_FOLDER = Path("~/bmt").expanduser()
@@ -11,9 +16,14 @@ ARCHIVE_FOLDER = Path("/mnt/...")
 
 IGNORE_FILE = Path("~/bmt/")
 KEEP_ARCHIVED = Path("~/bmt/")
+
+ALL_FILES = Path("~/bmt/")
 SYNCED_FILES = Path("~/bmt/")
+ARCHIVED_FILES = Path("~/bmt/")
 
 KEEP_AGE = timedelta(days=60)
+
+CONFIG_DATA = Path("~/bmt/...")
 # ARCHIVED_LIST = ARCHIVE_FOLDER.joinpath("")
 
 
@@ -30,22 +40,131 @@ def get_files(folder, ignores):
     return files
 
 
-def archive():
+def eject(drive):
+    r = subprocess.run(["sudo", "eject", drive], capture_output=True)
+    if r.returncode:
+        logging.error("A külső merevlemez (%s) leválasztása sikertelen. (kód: %d, '%s', '%s')",
+                      drive, r.returncode, r.stdout, r.stderr)
+        raise OSError(f"Failed to eject drive {drive}")
+
+
+def reconnect(drive):
+    r = subprocess.run(["findmnt", drive, "-J"], capture_output=True)
+    if r.returncode:
+        logging.warning("A merevlemez (%s) csatlakozottsági állapotának lekérése sikertelen. (kód: %d, '%s', '%s')",
+                        drive, r.returncode, r.stdout, r.stderr)
+    else:
+        response = json.loads(r.stdout)
+        if response["filesystems"]:
+            logging.warning("A külső merevlemez (%s) az már használatban van.", drive)
+            return
+
+    r = subprocess.run(["sudo", "eject", "-t", drive], capture_output=True)
+    if r.returncode:
+        logging.error("A külső merevlemez (%s) csatlakoztatása sikertelen. (kód: %d, '%s', '%s')",
+                      drive, r.returncode, r.stdout, r.stderr)
+        raise OSError(f"Failed to reconnect drive {drive}")
+
+
+def archive(freeup_needed=0):
+    with open(CONFIG_DATA) as f:
+        config = json.load(f)
+
+    try:
+        archive_device = config["device"]
+    except KeyError:
+        logging.error("Az archiváláshoz szükséges eszköz nincs specifikálva, a konfigurációs fájl "
+                      "nem tartalmmaza a 'device' mezőt.")
+        return
+
+    reconnect(archive_device)
+
     with open(IGNORE_FILE) as f:
         ignore_patterns = f.readlines()
+
+    global_files = get_file_info(ALL_FILES)
+
+    with TemporaryDirectory() as tempdir:
+        dir_path = Path(tempdir)
+        checkfile = dir_path.joinpath("checkfile.txt")
+        write_checkfile(checkfile, global_files)
+        differing_files_path = dir_path.joinpath("differ.txt")
+        not_archived_files_path = dir_path.joinpath("sync.txt")
+        deleted_files_path = dir_path.joinpath("deleted.txt")
+        r = subprocess.run(["rclone", "check", str(checkfile), ARCHIVE_FOLDER,
+                            "--checkcfile", "QuickXorHash",
+                            "--differ", str(differing_files_path),
+                            "--missing-on-dst", str(not_archived_files_path),
+                            "--missing-on-src", str(deleted_files_path)], capture_output=True)
+
+        with open(differing_files_path) as f:
+            copy_to_archive = f.read().splitlines()
+
+        with open(not_archived_files_path) as f:
+            copy_to_archive += f.read().splitlines()
+
+        with open(deleted_files_path) as f:
+            delete_from_archive = f.read().splitlines()
 
     synced_files = get_file_info(SYNCED_FILES)
 
     archived_files = get_files(ARCHIVE_FOLDER, ignore_patterns)
     local_files = get_files(LOCAL_FOLDER, ignore_patterns)
 
-    files_to_delete = []
-    files_to_copy = []
+    # files_to_delete = []
+    # files_to_copy = []
 
-    for file, data in local_files.items():
-        date, size = data
-        if file in archived_files and is_same_file(data, archived_files[file]):
-            if datetime.now() - date > KEEP_AGE:
-                files_to_delete.append(file)
+    delete_from_local = []
+
+    if freeup_needed < 0:
+        logging.warning("A kért felszabadítandó tárhely mérete negatív (%d).", freeup_needed)
+        freeup_needed = 0
+
+    if freeup_needed:
+        freed_up_space = 0
+
+        for _, name, size in sorted((date, name, size) for name, (date, size) in local_files.items()):
+            delete_from_local.append(name)
+            freed_up_space += size
+
+            if freed_up_space >= freeup_needed:
+                break
         else:
-            files_to_copy.append(file)
+            logging.warning("Nem lehetséges elegendő tárhely felszabadítása. Kért mennyiség: %d, "
+                            "teljesíthető: %d", freeup_needed, freed_up_space)
+
+    #     if file not in archived_files or not is_same_file(data, archived_files[file]):
+    #         files_to_copy.append(file)
+
+    #     if datetime.now() - date > KEEP_AGE and file in synced_files and is_same_file(data, synced_files[file]):
+    #         files_to_delete.append(file)
+    #         freed_up_space += size
+
+    # if freeup_needed > freed_up_space:
+    #     largest_files = sorted((file for file in local_files.keys() & synced_files.keys() - files_to_delete),
+    #                            key=lambda x: local_files[x][1], reverse=True)
+
+    #     for file in largest_files:
+    #         files_to_delete.append(file)
+    #         freed_up_space += local_files[file][1]
+    #         if freed_up_space >= freeup_needed:
+    #             break
+
+    # for file in copy_to_archive:
+    #     try:
+    #         copy2(LOCAL_FOLDER.joinpath(file), ARCHIVE_FOLDER.joinpath(file))
+    #     except Exception as e:
+    #         logging.error(f"Sikertelen másolás ({file}): {e}")
+    #         try:
+    #             files_to_delete.remove(file)
+    #         except ValueError:
+    #             pass
+    #         else:
+    #             logging.warning(f"A másolás miatt a fájl ({file}) nem fog törlésre kerülni.")
+    #         continue
+
+    #     archived_files[file] = local_files[file]
+
+    # for file in files_to_delete:
+
+    eject(archive_device)
--- util.py
+++ util.py
@@ -1,6 +1,10 @@
 import csv
 from datetime import datetime, timedelta
+import logging
 from typing import Dict, Iterable, Tuple, Union
+
+
+TIME_FORMAT = "%Y-%m-%d_%H.%M.%s,%f"
 
 class CSVDialect(csv.Dialect):
     delimiter: str = ":"
@@ -18,10 +22,32 @@ def read_csv(path):
         for line in reader:
             yield line
 
-def get_file_info(file) -> Dict[str, Tuple[datetime, int]]:
-    return {name: (datetime.fromtimestamp(time), int(size)) for name, time, size in read_csv(file)}
+def get_file_info(file) -> Dict[str, Tuple[str, datetime, int]]:
+    return {name: (hash, datetime.strptime(time, TIME_FORMAT), int(size)) for name, hash, time, size in read_csv(file)}
 
-def is_same_file(file1, file2):
-    date1, size1 = file1
-    date2, size2 = file2
-    return abs(date1 - date2) < timedelta(microseconds=10) and size1 == size2
+def write_csv(path, data: Iterable[Iterable]):
+    with open(path, newline="") as f:
+        writer = csv.writer(f, dialect=CSVDialect)
+        writer.writerows(data)
+
+def update_file_info(file, data: Dict[str, Tuple[str, datetime, int]]):
+    write_csv(file, ((name, hash, time.strftime(TIME_FORMAT), size) for name, (hash, time, size) in data.items()))
+
+def is_same_file(file1: Tuple[str, datetime, int], file2: Tuple[str, datetime, int]) -> bool:
+    hash1, date1, size1 = file1
+    hash2, date2, size2 = file2
+    if hash1 != hash2:
+        return False
+    
+    if size1 != size2:
+        logging.warning(f"Két fájl azonos hash-sel de eltérő mérettel rendelkezett. ({hash1}, {size1} != {size2})")
+        return False
+
+    if abs(date1 - date2) < timedelta(microseconds=10):
+        logging.warning("Két fájl azonos hash-sel és mérettel rendelkezett, de módosítási idejük eltérő. "
+                        f"({hash1}, {size1}, {date1.isoformat()} != {date2.isoformat()})")
+    return True
+
+def write_checkfile(path, data):
+    with open(path, "w", encoding="utf-8") as f:
+        f.writelines(f"{hash}  {name}\n" for name, (hash, _, _) in data.items())
